4d4c88662d2408ea487e4a0ec81dc3d89a2470ce
RuiTaborda <tabordarui@msn.com>
2020-12-11 01:42:47 +0000

    ssl work around
    ssl work around

--- APIClient.cs
+++ APIClient.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace APIClientPackage
@@ -14,32 +15,53 @@ namespace APIClientPackage
     public class APIClient
     {
         private LogInformation _log = null;
+
         private LogInformation _logTasks = null;
 
         private List<string> _commandLst = new List<string>();
         
         private List<string> _configurationLst = new List<string>();
 
-        //private ApiConnectorClient _apiConnectorClient = null;
+        private HttpClient _client = null;
 
-        private static readonly HttpClient client = new HttpClient();
+        private string _webSocketPath = "";
 
-        string _webSocketPath = "";
-
-        string _restPath = "";
+        private string _restPath = "";
 
         public APIClient(ref LogInformation log, ref LogInformation logTasks)
         {
             _log = log;
-            _logTasks = logTasks;
+            _logTasks = logTasks;            
             LoadConfiguration();
+            SSLClient();
+        }
+
+        private int SSLClient()
+        {            
+            try
+            {
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                //clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                clientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                //clientHandler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                //clientHandler.CookieContainer = container;
+                
+                // Pass the handler to httpclient(from you are calling api)
+                _client = new HttpClient(clientHandler);
+                
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                return -1;
+            }
         }
 
         public void LoadConfiguration()
         {
             try
             {
-                //List<string> configurationLst = new List<string>();
                 _configurationLst.Clear();
                 _configurationLst = File.ReadAllLines("GeneralConfiguration.cfg").ToList();
                 _webSocketPath = _configurationLst[0];
@@ -55,7 +77,6 @@ namespace APIClientPackage
         {
             try
             {
-                //List<string> configurationLst = new List<string>();
                 _commandLst.Clear();
                 _commandLst = File.ReadAllLines("CommandsConfiguration.cfg").ToList();
             }
@@ -78,8 +99,8 @@ namespace APIClientPackage
                 {
                     Console.WriteLine(string.Format("{0}->{1}",counter++,val));
                 }
-                Console.WriteLine(" ");
                 Console.WriteLine("ret -> Return to main menu.");   
+                Console.WriteLine(" ");
             }
             catch (Exception ex)
             {
@@ -101,8 +122,8 @@ namespace APIClientPackage
                     Console.WriteLine(string.Format("{0}->{1}",counter++,val));
                 }
 
-                Console.WriteLine(" ");
                 Console.WriteLine("ret -> Return to main menu.");    
+                Console.WriteLine(" ");
             }
             catch (Exception ex)
             {
@@ -120,8 +141,10 @@ namespace APIClientPackage
                 Console.WriteLine("# # # FriendUp Core API Tester");
                 Console.WriteLine("1 -> See general configuration (file GeneralConfiguration.cfg).");
                 Console.WriteLine("2 -> See commands calls configuration (file CommandsConfiguration.cfg).");
-                Console.WriteLine("EX#N -> Execute command number N");
+                Console.WriteLine("get#N -> Execute command number N (Type GET)");
+                Console.WriteLine("post#N -> Execute command number N (Type POST)");
                 Console.WriteLine("exit -> Terminate Program.");            
+                Console.WriteLine(" ");
                 Console.WriteLine("OPTIONS: ");
             }
             catch (Exception ex)
@@ -130,25 +153,82 @@ namespace APIClientPackage
             }
         }
 
-        private async Task ExecuteCommandAsync(int line)
+        private async Task ExecuteGETCommandAsync(int line)
         {
             try
             {
                 _logTasks.LogMsgOnly(" ");
-                _logTasks.LogMsgOnly("@ @ @ @ @ @ @ @ @ @ @ @ EXECUTE REST @ @ @ @ @ @ @ @ @ @ @ @");
+                _logTasks.LogMsgOnly("@ @ @ @ @ @ @ @ @ @ @ @ EXECUTE GET REST @ @ @ @ @ @ @ @ @ @ @ @");
                 _logTasks.LogMsgOnly(string.Format("Path:{0}",_restPath));
                 _logTasks.LogMsgOnly(string.Format("Agent:{0}","User-Agent"));
                 _logTasks.LogMsgOnly(string.Format("With:{0}",".NET Foundation Repository Reporter"));
                 _logTasks.LogMsgOnly(string.Format("Command:{0}",_commandLst[line]));
 
-                client.DefaultRequestHeaders.Accept.Clear();                
-                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_commandLst[line]));
-                var stringTask = client.GetStringAsync(_restPath);
+                _client.BaseAddress = new Uri(_restPath);
 
-                var msg = await stringTask;
-                _logTasks.LogMsgOnly(string.Format("RESULT:{0}",msg));
+                // Add an Accept header for JSON format.  
+                //_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                // Add an Accept header for HTML format.  
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                HttpResponseMessage response = _client.GetAsync(_commandLst[line]).Result;  // Blocking call!  
+
+                if (response.IsSuccessStatusCode)
+                {
+                    _logTasks.LogMsgOnly("Request Message Information:- \n\n" + response.RequestMessage + "\n");
+                    _logTasks.LogMsgOnly("Response Message Header \n\n" + response.Content.Headers + "\n");
+                }
+                else
+                {
+                    _logTasks.LogMsgOnly("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                if(ex.HelpLink!=null)
+                {
+                    _log.Error(ex.HelpLink);
+                }
+                if(ex.StackTrace!=null)
+                {
+                    _log.Error(ex.StackTrace);
+                }
+                if(ex.TargetSite!=null)
+                {
+                    _log.Error(ex.TargetSite.ToString());                
+                }
+            }
+        }
+
+        private async Task ExecutePOSTCommandAsync(int line )
+        {
+            try
+            {
+                string[] dataLineAll = _commandLst[line].Split(' ');
+                string url = dataLineAll[0];
+                string paramters = dataLineAll[1];
+
+                _logTasks.LogMsgOnly(" ");
+                _logTasks.LogMsgOnly("@ @ @ @ @ @ @ @ @ @ @ @ EXECUTE POST REST @ @ @ @ @ @ @ @ @ @ @ @");
+                _logTasks.LogMsgOnly(string.Format("Path:{0}",_restPath));
+                _logTasks.LogMsgOnly(string.Format("Agent:{0}","User-Agent"));
+                _logTasks.LogMsgOnly(string.Format("With:{0}",".NET Foundation Repository Reporter"));
+                _logTasks.LogMsgOnly(string.Format("Command:{0}",_commandLst[line]));
+
+                _client.BaseAddress = new Uri(_restPath);
+                System.Net.Http.HttpContent content = new StringContent(paramters, UTF8Encoding.UTF8, "application/json");
+                var response = _client.PostAsync(url, content).Result;                      
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    _logTasks.LogMsgOnly("Request Message Information:- \n\n" + response.RequestMessage + "\n");
+                    _logTasks.LogMsgOnly("Response Message Header \n\n" + response.Content.Headers + "\n");
+                }
+                else
+                {
+                    _logTasks.LogMsgOnly("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
             }
             catch (Exception ex)
             {
@@ -172,12 +252,12 @@ namespace APIClientPackage
         {
                 try
                 {
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(
+                    _client.DefaultRequestHeaders.Accept.Clear();
+                    _client.DefaultRequestHeaders.Accept.Add(
                         new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-                    client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+                    _client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-                    var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+                    var stringTask = _client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
 
                     var msg = await stringTask;
                     Console.Write(msg);
@@ -223,12 +303,18 @@ namespace APIClientPackage
                 {
                     running=false;
                 }
-                else if(commandStr.ToLower().Contains("ex#"))
+                else if(commandStr.ToLower().Contains("get#"))
                 {
                     string[] dataLineAll = commandStr.Split('#');
 
-                    await ExecuteCommandAsync(int.Parse(dataLineAll[1]));
-                }                   
+                    await ExecuteGETCommandAsync(int.Parse(dataLineAll[1]));
+                }   
+                else if(commandStr.ToLower().Contains("post#"))
+                {
+                    string[] dataLineAll = commandStr.Split('#');
+
+                    await ExecutePOSTCommandAsync(int.Parse(dataLineAll[1]));
+                }                      
                 else if(menuToPrint == 1) // Configuration Info
                 {
                     if(commandStr.ToLower() == "ret")
